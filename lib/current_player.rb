cd220c2268f482584b92bb8f01d7a5c14c49f798
Danielle Bergbower <dkw5011@gmail.com>
2019-03-17 21:59:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,10 @@ def turn_count(board)
   count
 end
 def current_player(board)
-  if (turn_count(board) % 2) = "0"
-    puts "O"
-  else 
+  if (turn_count(board) % 2) == "0"
     puts "X"
+  else 
+    puts "O"
   end
 end
 
