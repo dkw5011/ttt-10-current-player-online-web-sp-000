4978825553bbcc6df24810bcb865eed459e254aa
Danielle Bergbower <dkw5011@gmail.com>
2019-03-17 21:52:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,12 +6,12 @@ def turn_count(board)
     end
   end
   count
-
+end
 def current_player(board)
-  if turn_count(board) % 2 == 0 
+  if turn_count(board).integer % 2 == 0 
     puts "O"
   else 
     puts "X"
   end
 end
-end
+
